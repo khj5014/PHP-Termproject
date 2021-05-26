455176e257a4d56f6b19feb5b4b135be1db678dc
jimin <45274316+FeelingXD@users.noreply.github.com>
2021-05-26 16:55:36 +0900

    db.php set

--- index.php
+++ index.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/6c03ff6e85.js" crossorigin="anonymous"></script>
-     <!-- 스크립트 주소-->
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script><!-- 제이쿼리-->
-    <script type="text/javascript" src="./js/common.js"></script>
+    <script src="https://kit.fontawesome.com/6c03ff6e85.js" crossorigin="anonymous"></script><!--폰트 어썸 !-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script><!-- 제이쿼리 -->
+    <script type="text/javascript" src="./js/common.js"></script> <!-- 개인 스크립트 -->
     <link rel="stylesheet" href="./css/style.css"> <!--  스타일 시트 주소 -->
     <title>관리페이지</title>
 </head>
@@ -21,7 +20,7 @@
         if(isset($_SESSION['userid']))
           include "status.php";
         else
-        include "loginbox.php";
+          include "loginbox.php";
         ?>
     </section>
     <footer>
