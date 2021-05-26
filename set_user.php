64dd1ab19e6628f406758c110f84cab6a77aab09
jimin <45274316+FeelingXD@users.noreply.github.com>
2021-05-27 03:00:54 +0900

    SET_USER APPLY DROPPED '@'

--- set_user.php
+++ set_user.php
@@ -1,6 +1,26 @@
-<div>
+<style>
 
+</style>
+<div>
+    <form name ="create_user"action="create_user.php" method="POST">
+        <ul>
+            <div>
+                <input type="text" name="id" placeholder="ID"></INPUT>
+            </div>
+            <div>
+                <input type="password" name="pw1" placeholder="PW">
+            </div>
+            <div>
+                <input type="password" name="pw2" placeholder="PW">
+            </div>
+            <div>
+                <input type="text" name="email_front">@<input type="text" name="email_end">
+            </div>
+        </ul>
+        <button></button>
+    </form>
 </div>
+
 <div>
 
 </div>
